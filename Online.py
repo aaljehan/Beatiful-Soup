ea9bb19bcff00863e75eb5559e9446990b004709
aaljehan <97995046+aaljehan@users.noreply.github.com>
2022-04-11 10:45:44 -0400

    my second change

--- Online.py
+++ Online.py
@@ -6,14 +6,13 @@ import beautifulsoup;
 def getJobList():
     url = 'https://www.indeed.com/jobs?q={role}&l={location}'
     # Complete the missing part of this function here
-    #dasha was here
-#123f
-
+    r = request.get(url)
+    #return html
 #save data in csv file
 def saveDataInCSV():
     #Complete the missing part of this function here
     print("Saving data to csv")
-
+#saving csv file
 #main function
 def main():
     # Write a code here to get job location and role from user e.g. role = input()
